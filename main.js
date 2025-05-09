864e90a220414ebfee7b32857bcc135274757775
Andy Hall <andersonhall@gmail.com>
2025-05-09 18:38:52 -0500

    implement random colors and grid size config

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 let gridContainer = document.querySelector(".grid-container");
 const button = document.querySelector("button");
 
-// function getRandomColor() {
-//   const letters = "0123456789ABCDEF";
-//   let color = "#";
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
+function getRandomColor() {
+  const R = Math.floor(Math.random() * 255) + 1;
+  const G = Math.floor(Math.random() * 255) + 1;
+  const B = Math.floor(Math.random() * 255) + 1;
+  return `rgb(${R},${G},${B})`;
+}
 
 function createGrid(value) {
   const percent = 100 / value;
@@ -18,7 +16,7 @@ function createGrid(value) {
     div.style.height = `${percent}%`;
     div.classList.add("grid-item");
     div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = "black";
+      div.style.backgroundColor = getRandomColor();
     });
     gridContainer.appendChild(div);
   }
